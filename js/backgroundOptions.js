16fa17f22426020bca59df9a61991a9b5362775a
YippeeYi <Du_Yippee@outlook.com>
2026-06-29 00:15:45 +0800

    add

--- js/backgroundOptions.js
+++ js/backgroundOptions.js
@@ -13,5 +13,13 @@
         meta: `Photo by <a href="https://unsplash.com/@asoggetti?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Alessio Soggetti</a> on <a href="https://unsplash.com/photos/mountains-covered-with-fogs-gdE-5Oui1Y0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>`,
         image: "images/backgrounds/blue-sky-mountain.jpg",
         preview: "url('images/backgrounds/blue-sky-mountain.jpg')"
+    },
+    {
+        id: "gray-skull",
+        category: "\u98ce\u666f",
+        label: "\u98ce\u666f",
+        meta: "灰色骷髅头",
+        image: "images/backgrounds/blue-sky-mountain.jpg",
+        preview: "url('images/backgrounds/gray-skull.png')"
     }
 ];
