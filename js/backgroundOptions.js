6dd9a61d19e219f3212bf9a9cc8d30bb51bee919
YippeeYi <Du_Yippee@outlook.com>
2026-07-13 15:32:11 +0800

    add

--- js/backgroundOptions.js
+++ js/backgroundOptions.js
@@ -21,13 +21,5 @@
         meta: 'Photo by <a href="https://unsplash.com/@agneserudzite?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Agnese Rudzīte</a> on <a href="https://unsplash.com/photos/pink-and-orange-clouds-against-a-pale-sky-at-sunset-bziUIonXyI4?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>',
         image: "images/backgrounds/cloud.jpg",
         preview: "url('images/backgrounds/cloud.jpg')"
-    },
-    {
-        id: "star",
-        category: "\u98ce\u666f",
-        label: "星",
-        meta: 'Photo by <a href="https://unsplash.com/@evgenit?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Evgeni Tcherkasski</a> on <a href="https://unsplash.com/photos/a-glowing-red-nebula-in-deep-space-with-many-stars-aVqaHb7rNfE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>',
-        image: "images/backgrounds/star.jpg",
-        preview: "url('images/backgrounds/star.jpg')"
     }
 ];
